24512758a0689449e615caf8ebe33b0f874161bc
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-06 23:31:42 -0700

    WIP displayMovieDetails and rearrange some comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
-let secreOfTheThing = new Movie("Secret of the Thing", [130, 330, 530], "200", "R", 15)
-let theWolfOfWallstreet = new Movie("The Wolf On Wall Street", [133, 100,], "1000", "R", 100)
-let noseferatu = new Movie("Noseferatu", [2000, 2300, 2330], "120", "XXX", 13)
-let theLionTeam = new Movie("The Lion Team", [430, 530, 730], "90", "G", 12)
-let deepFeelings = new Movie("Deep Feelings", [930, 1030, 1130, 1230], "240", "PG-13", 11)
-
-let movieTitles = [secreOfTheThing, theWolfOfWallstreet, noseferatu, theLionTeam, deepFeelings]
-let movieString = ""
-
+//Movie Constructor
 function Movie(movieName, movieShowings, movieRating, moviePrice, movieRunTime) {
   this.movieName = movieName
   this.movieShowings = movieShowings
@@ -14,56 +6,41 @@ function Movie(movieName, movieShowings, movieRating, moviePrice, movieRunTime) 
   this.movieRating = movieRating
   this.moviePrice = moviePrice
 }
-//by here the database has been made of Movie objects
-//Trying out
-// Business Logic for MovieDetails ---------
-function MovieDetails() {
-  this.movies = {};
-  this.currentId = 0;
-}
+//Movie 'Database'
+let secreOfTheThing = new Movie("Secret of the Thing", [130, 330, 530], "200", "R", 15)
+let theWolfOfWallstreet = new Movie("The Wolf On Wall Street", [133, 100,], "1000", "R", 100)
+let noseferatu = new Movie("Noseferatu", [2000, 2300, 2330], "120", "XXX", 13)
+let theLionTeam = new Movie("The Lion Team", [430, 530, 730], "90", "G", 12)
+let deepFeelings = new Movie("Deep Feelings", [930, 1030, 1130, 1230], "240", "PG-13", 11)
+//by here the 'database' has been made/populated of Movie objects
+/* 
+we effectively wind up with:
 
-MovieDetails.prototype.assignId = function () {
-  this.currentId += 1;
-  return this.currentId;
+let theWolfOfWallStreet = {
+  movieName = "The Wolf of Wall Street",
+  movieShowings = [133, 100,],
+  movieRunTime = "1000",
+  movieRating = "R",
+  moviePrice = 100,
 }
+*/
 
-MovieDetails.prototype.addMovie = function (movie) {
-  movie.id = this.assignId();
-  this.movies[movie.id] = movie;
-}
-
-MovieDetails.prototype.findMovie = function (id) {
-  if (this.movies[id] != undefined) {
-    return this.movies[id];
-  }
-  return false;
-}
-
-MovieDetails.prototype.deleteMovie = function (id) {
-  if (this.movies[id] === undefined) {
-    return false;
-  }
-  delete this.movies[id];
-  return true;
-}
-//end of try out
-//UI LOGIC
-
+let movieTitles = [secreOfTheThing, theWolfOfWallstreet, noseferatu, theLionTeam, deepFeelings]
+//how do we make the line above 'loop' through database so we dont have to let movietitles? Can we?
 movieTitles.forEach(function (Title) {
   console.log(Title.movieName);
 })
 
 
-function displayMovieDetails(movieDetailsToDisplay) {
-  let movieList = $("ul#movie-list")
-  let htmlForMovieListInfo = ""
-  Object.keys(movieDetailsToDisplay.movies).forEach(function (key) {
-    const movie = movieDetailsToDisplay.findMovie(key)
-    htmlForMovieListInfo += "<li id=" + movie.id + ">" + movie.movieName + " " + movie.movieShowings + "</li>"
+function displayMovieDetails(movie) {
+  let MovieDetails = ""
+  movie.forEach(function (detail) {
+    let MovieDetails = MovieDetails + this.detail//obviously wrong. but close ask for help
   })
-  movieList.html(htmlForMovieListInfo)
+  console.log(MovieDetails)
 }
 
+displayMovieDetails
 
 
 
@@ -72,6 +49,19 @@ function displayMovieDetails(movieDetailsToDisplay) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // Business Logic for MovieDetails ---------
 function MovieDetails() {
   this.movies = {};
@@ -102,25 +92,10 @@ MovieDetails.prototype.deleteMovie = function (id) {
   delete this.movies[id];
   return true;
 }
-
-
-
-
-
-
-
-
-
-
+*/
 
 // Movie.forEach(function(movieName){
 
-// })
-
-
-// movieTitles.forEach(function (title) {
-//   title = this
-//   console.log(title);
 // })
 
 // movieTitles.forEach(function (title) {
@@ -130,18 +105,6 @@ MovieDetails.prototype.deleteMovie = function (id) {
 //   });
 //   console.log(title);
 // })
-
-/*
-function displayContactDetails(addressBookToDisplay) {
-  let contactsList = $('ul#contacts')
-  let htmlForContactInfo = ""
-  Object.keys(addressBookToDisplay.contacts).forEach(function (key) {
-    const contact = addressBookToDisplay.findContact(key)
-    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>"
-  })
-  contactsList.html(htmlForContactInfo)
-}
-*/
 
 // let tomatoes = { name: "Tomatoes", price: 2.99 };
 // let cucumbers = { name: "Cucumbers", price: 0.99 };
@@ -164,6 +127,18 @@ function displayContactDetails(addressBookToDisplay) {
 //   });
 //   console.log("\n");
 // });
+
+/*
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $('ul#contacts')
+  let htmlForContactInfo = ""
+  Object.keys(addressBookToDisplay.contacts).forEach(function (key) {
+    const contact = addressBookToDisplay.findContact(key)
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>"
+  })
+  contactsList.html(htmlForContactInfo)
+}
+*/
 
 /*
 function displayMovieDetails(addressBookToDisplay/*gonna change this parameter) {
